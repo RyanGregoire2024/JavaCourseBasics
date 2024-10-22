5c2b2569df8a0bf05d0ccb187cf9922f387f4b1e
RyanPSN <ryangregoire286@gmail.com>
2024-10-22 17:32:15 -0400

    Added Day-1

---
+++ Day-One/Day-One/src/GetMain/main/GetMain.java
@@ -0,0 +1,13 @@
+package GetMain.main;
+
+public class GetMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Line from App");
+        System.out.println("Second Line from App");
+        System.out.println("Third Line from App");
+
+        
+    }
+}
