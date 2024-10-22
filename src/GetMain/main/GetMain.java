756c2eea5da82e6ea1a9169fa99603ab105c9216
RyanPSN <ryangregoire286@gmail.com>
2024-10-22 19:48:13 -0400

    Added And Learned About Comments in Java:

--- src/GetMain/main/GetMain.java
+++ src/GetMain/main/GetMain.java
@@ -2,12 +2,31 @@ package GetMain.main;
 
 public class GetMain {
 
+    /*
+          -------------------------------------
+            - 1: Print Message To Console
+            - 2: Multi Line Comment That Comments List of Things to do
+          --------------------------------------
+    */
+
     public static void main(String[] args) {
 
         System.out.println("First Line from App");
         System.out.println("Second Line from App");
         System.out.println("Third Line from App");
 
-        
+
+
+        /**
+         // Java Doc Comment
+         *  1: Learn Java From Pluralsight
+         *  2: Spend 30 Minutes to 1 Hour Learning Java
+         *  3: Finish One Section Each Day Until Course is Complete
+         */
+
+        // Single Line Comment
+
+
     }
+
 }
