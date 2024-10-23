2e33398b7a690f570fe04f6dea14fb8ad716cf68
RyanPSN <ryangregoire286@gmail.com>
2024-10-23 12:12:30 -0400

    Added Day Two Code

---
+++ src/com/Learn/DayOne/DayTwo/DayTwo/src/Arithmetic/MathArithmetic.java
@@ -0,0 +1,17 @@
+package Arithmetic;
+
+public class MathArithmetic {
+    public static void main(String[] args) {
+
+        int sumBo = 10;
+        sumBo += 1;
+        System.out.println(sumBo);
+
+        int newSumValue = 4;
+        newSumValue -= 3;
+        System.out.println(newSumValue);
+
+
+
+    }
+}
---
+++ src/com/Learn/DayOne/DayTwo/DayTwo/src/Starting/Main.java
@@ -0,0 +1,17 @@
+package Starting;
+
+public class Main {
+    public static int myDataValue = 10 + 1;
+    public static int myInfo = myDataValue + 100;
+    public static int DATA_VAR = 79;
+
+    public static void main(String[] args) {
+
+
+        DATA_VAR = myInfo;
+
+
+        System.out.println(DATA_VAR);
+    }
+
+}
