06067cd9db48f28167160c750f6dfa6c4cc7c684
RyanPSN <ryangregoire286@gmail.com>
2024-10-24 10:20:09 -0400

    Added Day Three Basics

---
+++ src/com/Learn/Day-3/Day 3/src/com/Learn/Day/Three/Conditionals/Conditionals.java
@@ -0,0 +1,17 @@
+package com.Learn.Day.Three.Conditionals;
+
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+        int a = 2 + 8;
+        int b = 2 + 2;
+
+        if (a > b) {
+            System.out.println("a is Bigger");
+        } else {
+
+            System.out.println("b is Bigger");
+        }
+    }
+}
---
+++ src/com/Learn/Day-3/Day 3/src/com/Learn/Day/Three/DayThree.java
@@ -0,0 +1,15 @@
+package com.Learn.Day.Three;
+
+public class DayThree {
+
+    public static void main(String[] args) {
+
+        int value1 = 2 + 5;
+        int value2 = 2 + 3;
+        int value3 = value1 > value2 ? value1 : value2;
+
+
+        System.out.println(value3);
+
+    }
+}
